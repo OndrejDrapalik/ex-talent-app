7af0af96ab5cfbd3c7102faabff77d8493494d3f
Ondrej Drapalik <ondrej.drapalik@gmail.com>
2022-02-05 12:03:41 +0100

    fix: email inclouded in the form

--- components/AddTextForm.js
+++ components/AddTextForm.js
@@ -15,7 +15,7 @@ const MyTextInput = ({ label, ...props }) => {
       </label>
 
       <input
-        className="text-input w-auto h-7 border rounded-sm 
+        className="text-input w-[250px] h-7 border rounded-sm 
         bg-gray-100
         autofill:bg-white autofill:text-gray-700
         focus:bg-white text-gray-900  
@@ -60,7 +60,7 @@ const MySelect = ({ label, ...props }) => {
       <select
         {...field}
         {...props}
-        className="flex w-[170px] h-7 border rounded-sm  bg-gray-100"
+        className="flex w-[250px] h-7 border rounded-sm  bg-gray-100"
       />
       {meta.touched && meta.error ? (
         <div className="error absolute mt-[1px]">{meta.error}</div>
@@ -78,10 +78,14 @@ export default function AddTextForm({ zIndex }) {
           firstName: '',
           lastName: '',
           email: '',
+          location: '',
           company: '',
+          department: '',
           linkedIn: '',
           otherURL: '',
           shareEmail: false,
+          remoteWork: false,
+          relocation: false,
         }}
         validationSchema={Yup.object({
           firstName: Yup.string()
@@ -95,12 +99,10 @@ export default function AddTextForm({ zIndex }) {
           company: Yup.string()
             .oneOf(['Avast', 'Norton', 'Other'])
             .required('Required'),
-          linkedIn: Yup.string()
-            .matches(
-              /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
-              'Enter correct url!'
-            )
-            .required('Please enter website'),
+          linkedIn: Yup.string().matches(
+            /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+            'Enter correct url'
+          ),
         })}
         onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
@@ -111,15 +113,17 @@ export default function AddTextForm({ zIndex }) {
       >
         <>
           <div
+            // blue background
             className={`fixed top-0 right-0 bottom-0 left-0 w-full h-full ${zIndex}
                           bg-secondary opacity-100 
                           cursor-default`}
           ></div>
           <div
-            className={`${zIndex} flex px-14 pt-14 pb-14 rounded-lg
+            // white window
+            className={`${zIndex} absolute px-14 pt-14 pb-14 rounded-lg
                   bg-primary  shadow-md`}
           >
-            <Form className="flex flex-col gap-8 ">
+            <Form className="flex flex-col gap-8">
               <div className="flex flex-row gap-6">
                 <div className="flex flex-col ">
                   <MyTextInput
@@ -139,64 +143,95 @@ export default function AddTextForm({ zIndex }) {
                 </div>
               </div>
 
-              <div className="flex flex-col">
-                <MyTextInput
-                  label="Email Address"
-                  name="email"
-                  type="email"
-                  placeholder="ondrej.drapalik@gmail.com"
-                />
+              <div className="flex flex-row justify-between items-end">
+                <div className="flex flex-col">
+                  <MyTextInput
+                    label="Email Address"
+                    name="email"
+                    type="email"
+                    placeholder="ondrej.drapalik@gmail.com"
+                  />
+                </div>
+                <MySelect
+                  label="Location"
+                  name="location"
+                  className="flex flex-col "
+                >
+                  <option value="">My location</option>
+                  <option value="designer">Designer</option>
+                  <option value="development">Developer</option>
+                  <option value="product">Product Manager</option>
+                  <option value="other">Other</option>
+                </MySelect>
               </div>
-              <div className="flex flex-row gap-6">
+              <div className="flex flex-row justify-between">
                 <MySelect
                   label="Company"
                   name="company"
                   className="flex flex-col "
                 >
-                  <option value="">Select a company</option>
+                  <option value="">My company</option>
                   <option value="Avast">Avast</option>
                   <option value="Norton">Norton</option>
                   <option value="Other">Other</option>
                 </MySelect>
 
-                {/* <MySelect
+                <MySelect
                   label="Department"
                   name="department"
                   className="flex flex-col "
                 >
-                  <option value="">Select a job type</option>
+                  <option value="">My department</option>
                   <option value="designer">Designer</option>
                   <option value="development">Developer</option>
                   <option value="product">Product Manager</option>
                   <option value="other">Other</option>
-                </MySelect> */}
+                </MySelect>
               </div>
 
-              <div className="flex flex-col">
-                <MyTextInput
-                  label="LinkedIn"
-                  name="linkedIn"
-                  type="text"
-                  placeholder="linkedin.com/in/ondrej-drapalik-65a071a5/"
-                />
+              <div className="flex flex-row justify-between">
+                <div className="flex flex-col">
+                  <MyTextInput
+                    label="LinkedIn"
+                    name="linkedIn"
+                    type="text"
+                    placeholder="linkedin.com/in/ondrej-drapalik-65a071a5/"
+                  />
+                </div>
+
+                <div className="flex flex-col">
+                  <MyTextInput
+                    label="Other URL"
+                    name="otherURL"
+                    type="text"
+                    placeholder="github.com/OndrejDrapalik"
+                  />
+                </div>
               </div>
 
-              <div className="flex flex-col">
-                <MyTextInput
-                  label="Other URL"
-                  name="otherURL"
-                  type="text"
-                  placeholder="github.com/OndrejDrapalik"
-                />
-              </div>
-
-              <MyCheckbox
-                name="shareEmail"
-                className="appearance-none border-2 bg-contain  w-4 h-4 cursor-pointer rounded-sm
+              <div>
+                <MyCheckbox
+                  name="shareEmail"
+                  className="appearance-none border-2 bg-contain  w-4 h-4 cursor-pointer rounded-sm
                       checked:bg-check-box checked:bg-secondary checked:border-secondary"
-              >
-                <div>I wish to share my email with others publicly.</div>
-              </MyCheckbox>
+                >
+                  <div>I wish to share my email with others publicly.</div>
+                </MyCheckbox>
+                <MyCheckbox
+                  name="remoteWork"
+                  className="appearance-none border-2 bg-contain  w-4 h-4 cursor-pointer rounded-sm
+                      checked:bg-check-box checked:bg-secondary checked:border-secondary"
+                >
+                  <div>I am open to remote work.</div>
+                </MyCheckbox>
+                <MyCheckbox
+                  name="relocation"
+                  className="appearance-none border-2 bg-contain  w-4 h-4 cursor-pointer rounded-sm
+                      checked:bg-check-box checked:bg-secondary checked:border-secondary"
+                >
+                  <div>I am open to relocation.</div>
+                </MyCheckbox>
+              </div>
 
               <div className="flex justify-between items-center gap-4">
                 <Link href={'/'} passHref>
