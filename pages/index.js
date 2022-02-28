fa81aac1b70a8a13a9d17c1e2aea1f6f69cddc51
Ondrej Drapalik <ondrej.drapalik@gmail.com>
2022-02-28 20:27:39 +0100

    feat: add SSR

--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,21 @@ import PostFeed from '../components/PostFeed';
 import { firestore, postToJSON } from '../lib/firebase';
 
 // SSR is used for inital render for better UX
-// export async function getServerSideProps() {
-//   const postQuery = firestore
-//     .collectionGroup('entry_collection')
-//     .orderBy('id', 'asc');
+export async function getServerSideProps() {
+  const postQuery = firestore
+    .collectionGroup('entry_collection')
+    .orderBy('id', 'asc');
 
-//   const firstLoad = (await postQuery.get()).docs.map(postToJSON);
+  const firstLoad = (await postQuery.get()).docs.map(postToJSON);
 
-//   return {
-//     props: { firstLoad },
-//   };
-// }
+  return {
+    props: { firstLoad },
+  };
+}
 
 export default function Home(props) {
-  // const [firstData] = useState(props.firstLoad);
+  const [firstData] = useState(props.firstLoad);
+  const [entries, setEntries] = useState([]);
   const [shuffle, setShuffle] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -31,15 +32,15 @@ export default function Home(props) {
   const [remoteSelected, setRemoteSelected] = useState(false);
   const [relocationSelected, setRelocationSlected] = useState(false);
 
+  // Swap firstLoad data with entries that are further shuffled once all data are fetched
   useEffect(() => {
-    const fn = async () => {
-      const postQuery = firestore
-        .collectionGroup('entry_collection')
-        .orderBy('id', 'asc');
+    async function fetchData() {
+      let response = await firstData;
+      setEntries(response);
+      setLoading(false);
 
-      const data = (await postQuery.get()).docs.map(postToJSON);
-
-      const dataSortable = [...data].map((x) => {
+      // Randomise entries and save it as a shuffle state
+      const dataSortable = [...response].map((x) => {
         return {
           ...x,
           sortKey: Math.random(),
@@ -49,14 +50,14 @@ export default function Home(props) {
       dataSortable.sort((a, b) => a.sortKey - b.sortKey);
 
       setShuffle(dataSortable);
-    };
+    }
 
-    fn();
-  }, []);
+    fetchData();
+  }, [firstData]);
 
   // Clean & order entries data for usage in the filters
   useEffect(() => {
-    const mapCountries = [...shuffle].map((item) => item.values.justCountry);
+    const mapCountries = [...entries].map((item) => item.values.justCountry);
     const filterCountries = mapCountries
       .filter((item, index) => mapCountries.indexOf(item) >= index)
       .filter((item) => item !== '');
@@ -64,23 +65,23 @@ export default function Home(props) {
     const orderCountries = filterCountries.sort();
     setCleanCountry(orderCountries);
 
-    const mapDepartments = [...shuffle].map((item) => item.values.department);
+    const mapDepartments = [...entries].map((item) => item.values.department);
     const filterDepartments = mapDepartments.filter(
       (item, index) => mapDepartments.indexOf(item) >= index
     );
     const orderDepartments = filterDepartments.sort();
     setCleanDepartment(orderDepartments);
-  }, [shuffle]);
+  }, [entries]);
 
   // Filter rendering logic
   useEffect(() => {
     let filtered = [];
 
     const countryFilter = () => {
-      const filterCountry = shuffle.filter(
+      const filterCountry = entries.filter(
         (i) => countrySelected === i.values.justCountry
       );
-      countrySelected ? (filtered = filterCountry) : (filtered = shuffle);
+      countrySelected ? (filtered = filterCountry) : (filtered = entries);
       setShuffle(filtered);
     };
 
@@ -115,14 +116,14 @@ export default function Home(props) {
   }, [
     countrySelected,
     departmentSelected,
-    shuffle,
+    entries,
     relocationSelected,
     remoteSelected,
   ]);
 
-  const firstHalf = Math.floor(shuffle.length / 2);
+  const firstHalf = Math.floor(entries.length / 2);
   const colA = shuffle.slice(0, firstHalf);
-  const colB = shuffle.slice(firstHalf, shuffle.length);
+  const colB = shuffle.slice(firstHalf, entries.length);
   console.log(loading ? 'loading firstLoad' : 'loading SHUFFLE');
 
   return (
@@ -133,7 +134,7 @@ export default function Home(props) {
         className="m-auto max-w-6xl px-[5vw] md:px-10"
       >
         <h1 className="pt-20 pb-10 text-3xl sm:text-4xl md:pb-10 md:text-5xl">
-          Please meet some talented people who’ve worked at Avast.
+          SSR. Please meet some talented people who’ve worked at Avast.
         </h1>
         <div
           // Filter group
