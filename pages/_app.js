b4bf2ff7fcd248363637a081861b54a8c836da20
Ondrej Drapalik <ondrej.drapalik@gmail.com>
2022-04-02 18:08:17 +0200

    feat: add cringy footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
-import "../styles/index.css";
-import Navbar from "../components/Navbar";
-import AppState from "../lib/contexts/AppState";
+import '../styles/index.css';
+import Navbar from '../components/Navbar';
+import AppState from '../lib/contexts/AppState';
+import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AppState>
       <Component {...pageProps} />
+      <Footer />
     </AppState>
   );
 }
